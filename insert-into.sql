c6e356eb9781011933b815584b98deb046c8cc0d
FlavioH13 <flavio.henrique375@gmail.com>
2024-07-26 10:28:40 -0300

    Add files via upload

---
+++ insert-into.sql
@@ -0,0 +1,24 @@
+insert into `db_senac`.`tbl_pessoas` (
+    `nome`,
+    `rg`,
+    `cpf`,
+    `data_nascimento`,
+    `email`,
+    `whatsapp`,
+    `senha`,
+    `estado_civil`,
+    `genero`,
+    `naturalidade`
+) values (
+    'Fernanda',
+    '12.345.678-9',
+    '123.456.789-00',
+    '01/02/-1982',
+    'fernandinha02@gmail.com',
+    '+965 (49)165289-*56239',
+    '123456',
+    'solteira',
+    'feminino',
+    'goiás / mato grosso do sul'
+);
+
